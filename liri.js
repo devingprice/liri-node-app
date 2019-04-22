2354b750acd67f71521827f33d12a672dfaf0111
devingprice <devingprice@gmail.com>
2019-04-22 11:13:24 -0400

    Add concertThis request

--- liri.js
+++ liri.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+var axios = require("axios");
 
 var command = process.argv[2];
-var name = process.argv.splice(3, process.argv.length-1).join(' ');
+var name = process.argv.splice(3, process.argv.length-1).join('_');
 console.log( name )
 
 switch (command) {
     case `concert-this`:
+        concertThis(name);
         break;
 
     case 'spotify-this-song':
@@ -17,4 +19,11 @@ switch (command) {
 
     case 'do-what-it-says':
         break;
+}
+
+function concertThis(name){
+    var url = "https://rest.bandsintown.com/artists/" + name + "/events?app_id=codingbootcamp";
+    axios.get(url).then(function(response){
+        console.log( response.data )
+    })
 }
