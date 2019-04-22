81cfc358f66a994d8e462bac27dec6bfccc10b78
devingprice <devingprice@gmail.com>
2019-04-22 10:56:55 -0400

    Add command switch

--- liri.js
+++ liri.js
@@ -1,2 +1,20 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+
+var command = process.argv[2];
+var name = process.argv.splice(3, process.argv.length-1).join(' ');
+console.log( name )
+
+switch (command) {
+    case `concert-this`:
+        break;
+
+    case 'spotify-this-song':
+        break;
+
+    case 'movie-this':
+        break;
+
+    case 'do-what-it-says':
+        break;
+}
